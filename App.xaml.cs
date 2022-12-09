b4f91919299e3af43e27f5180a046643242802f4
Adrián Pelayo Avilés <adrian.pelayo@trentia.net>
2022-12-09 14:19:52 +0100

    First commit + Reset button

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MAUI_learn_module;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI_learn_module;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
